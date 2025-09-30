d0a0d8bee50db2ad67d09121cdd9425a5b23b58f
Shira Giladi <121168125+shiragildi1@users.noreply.github.com>
2025-09-30 20:03:40 +0300

    Change API_BASE environment variable for React

--- Client/src/services/authService.js
+++ Client/src/services/authService.js
@@ -19,7 +19,8 @@
 //   }).then((res) => res.json());
 // }
 
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "http://localhost:8080";
+const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:8080";
+
 export function sendGoogleToken(idToken) {
   return fetch(`${API_BASE}/api/auth/google`, {
     method: "POST",
